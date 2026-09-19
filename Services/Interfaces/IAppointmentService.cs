f8751e6137a3ed8a8807ddb440eaee635c62337e
Syabonga Njabulo Duma <njabulod189@gmail.com>
2026-09-19 18:33:48 +0200

    Update IAppointmentService.cs

--- Services/Interfaces/IAppointmentService.cs
+++ Services/Interfaces/IAppointmentService.cs
@@ -1,24 +1,32 @@
-﻿using PersonalProject.Models.DTOs;
+using PersonalProject.Models.DTOs;
 
 namespace PersonalProject.Services.Interfaces
 {
     public interface IAppointmentService
     {
-        Task<AppointmentResponseDto> CreateAsync(
-            CreateAppointmentDto dto,
+        Task<AppointmentResponseDto>
+            CreateAsync(
+                CreateAppointmentDto dto,
+                Guid performedByUserId
+            );
+
+        Task<AppointmentResponseDto>
+            UpdateAsync(
+                Guid appointmentId,
+                UpdateAppointmentDto dto,
+                Guid performedByUserId
+            );
+
+        Task DeleteAsync(
+            Guid appointmentId,
             Guid performedByUserId
         );
 
-        Task<AppointmentResponseDto> UpdateAsync(
-            Guid appointmentId,
-            UpdateAppointmentDto dto,
-            Guid performedByUserId
-        );
-
-        Task<AppointmentResponseDto?> GetByIdAsync(
-            Guid appointmentId,
-            Guid performedByUserId
-        );
+        Task<AppointmentResponseDto?>
+            GetByIdAsync(
+                Guid appointmentId,
+                Guid performedByUserId
+            );
 
         Task<List<AppointmentResponseDto>>
             GetClinicAppointmentsAsync(
